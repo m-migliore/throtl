f355a9c6f205195badcd5768ba2655ae6168c908
Migliore <Matt.Migliore@corp.oproot.opco.com>
2019-05-23 11:27:54 -0400

    add table start

--- src/components/RaceResults.js
+++ src/components/RaceResults.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class RaceResults extends Component {
+  componentDidMount() {
+    console.log(this.props.results);
+  }
 
   render() {
     const results = this.props.results
 
-    return (
+    return (<div>
       <ul>
         {results.map(result => <li key={result.number}>{result.number}</li>)}
       </ul>
-    );
+
+      <table>
+        <tbody>
+          <tr>
+            <th>Position</th>
+            <th>Driver</th>
+            <th>Contructor</th>
+            <th>Points</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>);
   }
 
 }
-
 
 export default RaceResults;
