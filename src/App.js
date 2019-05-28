f8edd04a85af0adcae0645ba097dfc68210e48f8
Migliore <Matt.Migliore@corp.oproot.opco.com>
2019-05-28 16:35:27 -0400

    add error for year input

--- src/App.js
+++ src/App.js
@@ -1,61 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import { connect } from 'react-redux'
 import SeasonContainer from './components/SeasonContainer'
-import {fetchAllSeasonData} from './actions/actionCreators'
 
 class App extends Component {
-  state = {
-    season: ""
-  }
-
-  handleChange = e => {
-    this.setState({
-      season: e.target.value
-    })
-  }
-
-  handleSubmit = e => {
-    e.preventDefault()
-    console.log(this.state.season);
-    this.props.fetchAllSeasonData(this.state.season)
-  }
-
-  componentDidMount() {
-    this.props.fetchAllSeasonData("current")
-  }
 
   render() {
     return (
-      <div className={"container mx-auto"}>
-        <form onSubmit={this.handleSubmit}>
-          <input type="number" name="season" value={this.state.season} onChange={this.handleChange} />
-          <button type="submit">Load Season</button>
-        </form>
-        {this.props.loading ? <p>loading</p> :
-          <>
-            <h1>Formula 1 {this.props.seasonData.season} Season</h1>
-            <SeasonContainer />
-          </>
-        }
-
+      <div>
+        <SeasonContainer />
       </div>
     )
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    loading: state.loading,
-    seasonData: state.seasonData,
-    resultView: state.resultView
-  }
-}
 
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchAllSeasonData: season => dispatch(fetchAllSeasonData(season))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App;
--- src/components/SeasonContainer.js
+++ src/components/SeasonContainer.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import {fetchAllSeasonData} from '../actions/actionCreators'
 import StandingsContainer from './StandingsContainer'
 import RaceContainer from './RaceContainer'
 import RaceList from './RaceList'
 
 class SeasonContainer extends Component {
+  state = {
+    season: "",
+    error: ""
+  }
+
+  handleChange = e => {
+    this.setState({
+      season: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    debugger
+    if (this.state.season === "current" || (this.state.season <= new Date().getFullYear() &&  this.state.season > 1952)) {
+      this.props.fetchAllSeasonData(this.state.season)
+      this.setState({
+        season: "",
+        error: ""
+      })
+    } else {
+      this.setState({
+        error: `Enter year between 1952 and ${new Date().getFullYear()}`
+      })
+    }
+
+  }
+
+  componentDidMount() {
+    this.props.fetchAllSeasonData("current")
+  }
 
   render() {
     return (
-      <div>
-        <StandingsContainer />
-        {this.props.raceView ? <RaceContainer /> : <RaceList />}
+      <div className={"container mx-auto"}>
+        <form onSubmit={this.handleSubmit}>
+          <input type="number" name="season" value={this.state.season} onChange={this.handleChange} />
+          {this.state.error.length > 0 ? <p>{this.state.error}</p> : null}
+          <button type="submit">Load Season</button>
+        </form>
+        {this.props.loading ? <p>loading</p> :
+          <>
+            <h1>Formula 1 {this.props.seasonData.season} Season</h1>
+            <StandingsContainer />
+            {this.props.raceView ? <RaceContainer /> : <RaceList />}
+          </>
+        }
+
       </div>
     );
   }
@@ -19,9 +62,16 @@ class SeasonContainer extends Component {
 
 const mapStateToProps = state => {
   return {
+    loading: state.loading,
     seasonData: state.seasonData,
     raceView: state.raceView
   }
 }
 
-export default connect(mapStateToProps)(SeasonContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchAllSeasonData: season => dispatch(fetchAllSeasonData(season))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SeasonContainer);
