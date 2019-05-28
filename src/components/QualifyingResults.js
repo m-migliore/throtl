bab03e61ddfb4bec61c5d2babcf0a15701d64ab4
Migliore <Matt.Migliore@corp.oproot.opco.com>
2019-05-28 10:14:33 -0400

    add qual results container

---
+++ src/components/QualifyingResults.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class QualifyingResults extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Qualifying Results</h2>
+      </div>
+    );
+  }
+
+}
+
+export default QualifyingResults;
