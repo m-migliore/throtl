16d569ae1c79f10806ae21edb2393be96bdd8b29
Matt Migliore <mmigliore311@gmail.com>
2019-05-30 10:37:22 -0400

    add driver info to driver container

--- src/components/Driver/DriverContainer.js
+++ src/components/Driver/DriverContainer.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
+import Moment from 'react-moment'
 
 class DriverContainer extends Component {
 
   render() {
     if (this.props.driverData.driverId) {
+      const driver = this.props.driverData
       return (
-        <div>Driver {this.props.driverData.familyName}</div>
+        <div>
+          <h1>{driver.givenName + " " + driver.familyName} {driver.permanentNumber ? <span>{driver.permanentNumber }</span> : null}</h1>
+          <p><strong>DOB:</strong> <Moment date={driver.dateOfBrith} format="LLL"/></p>
+          <p><strong>Nationality:</strong> {driver.nationality}</p>
+        </div>
       );
     } else {
       return <Redirect to="/" />
