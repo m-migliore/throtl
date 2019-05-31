9576cd38ed238f0f297b8fab8968de4e940dc7ed
Matt Migliore <mmigliore311@gmail.com>
2019-05-31 14:18:09 -0400

    add beautified file

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,58 +1,74 @@
 export function fetchAllSeasonData(season) {
   return dispatch => {
-    dispatch({type: "START_ALL_SEASON_FETCH"})
+    dispatch({
+      type: "START_ALL_SEASON_FETCH"
+    })
     return Promise.all([
       dispatch(setSeason(season)),
       dispatch(fetchSeasonData(season)),
       dispatch(fetchDriverStandings(season)),
       dispatch(fetchConstructorStandings(season))
-    ]).then(() => dispatch({type:"COMPLETE_ALL_SEASON_FETCH"}))
+    ]).then(() => dispatch({
+      type: "COMPLETE_ALL_SEASON_FETCH"
+    }))
   }
 }
 
 export function setSeason(season) {
-  return dispatch => dispatch({type: "SET_SEASON", payload: season})
+  return dispatch => dispatch({
+    type: "SET_SEASON",
+    payload: season
+  })
 }
 
 export function fetchSeasonData(season) {
   return dispatch => {
-    dispatch({type: "FETCH_SEASON_DATA"})
-    return fetch(`http://ergast.com/api/f1/${season}.json`)
-    .then(r => r.json())
-    .then(data => {
-      return dispatch({
-        type: 'LOAD_SEASON_DATA',
-        payload:  data.MRData.RaceTable
-      })
+    dispatch({
+      type: "FETCH_SEASON_DATA"
     })
+    return fetch(`http://ergast.com/api/f1/${season}.json`)
+      .then(r => r.json())
+      .then(data => {
+        return dispatch({
+          type: 'LOAD_SEASON_DATA',
+          payload: data.MRData.RaceTable
+        })
+      })
   }
 
 }
 
 export function fetchDriverStandings(season) {
   return dispatch => {
-    dispatch({type: "FETCH_DRIVER_STANDINGS"})
+    dispatch({
+      type: "FETCH_DRIVER_STANDINGS"
+    })
     return fetch(`http://ergast.com/api/f1/${season}/driverStandings.json`)
-    .then(r => r.json())
-    .then(data =>  dispatch({
-      type: "LOAD_DRIVER_STANDINGS",
-      payload: data.MRData.StandingsTable
-    }))
+      .then(r => r.json())
+      .then(data => dispatch({
+        type: "LOAD_DRIVER_STANDINGS",
+        payload: data.MRData.StandingsTable
+      }))
   }
 }
 
 export function fetchConstructorStandings(season) {
   return dispatch => {
-    dispatch({type: "FETCH_CONSTRUCTOR_STANDINGS"})
+    dispatch({
+      type: "FETCH_CONSTRUCTOR_STANDINGS"
+    })
     if (season === "current" || season > 1957) {
       return fetch(`http://ergast.com/api/f1/${season}/constructorStandings.json`)
-      .then(r => r.json())
-      .then(data =>  dispatch({
-        type: "LOAD_CONSTRUCTOR_STANDINGS",
-        payload: data.MRData.StandingsTable
-      }))
+        .then(r => r.json())
+        .then(data => dispatch({
+          type: "LOAD_CONSTRUCTOR_STANDINGS",
+          payload: data.MRData.StandingsTable
+        }))
     } else {
-      return dispatch({type: "LOAD_CONSTRUCTOR_STANDINGS", payload: "Constructor standings not available prior to 1958 season."})
+      return dispatch({
+        type: "LOAD_CONSTRUCTOR_STANDINGS",
+        payload: "Constructor standings not available prior to 1958 season."
+      })
     }
   }
 }
@@ -60,38 +76,55 @@ export function fetchConstructorStandings(season) {
 // Fetch race results, qualifying results, pit data, and set circuit info for specific race in specific season
 export function fetchEventData(season, round) {
   return dispatch => {
-    dispatch({type: "FETCH_EVENT_DATA"})
+    dispatch({
+      type: "FETCH_EVENT_DATA"
+    })
     return Promise.all([
       dispatch(fetchRaceData(season, round)),
       dispatch(setCircuitData(round)),
       dispatch(fetchQualData(season, round)),
       dispatch(fetchPitData(season, round))
-    ]).then(() => dispatch({type:"COMPLETE_EVENT_DATA_FETCH"}))
+    ]).then(() => dispatch({
+      type: "COMPLETE_EVENT_DATA_FETCH"
+    }))
   }
 }
 
 export function fetchRaceData(season, round) {
   return dispatch => {
-    dispatch({type: "RACE_FETCH"})
-    return fetch(`http://ergast.com/api/f1/${season}/${round}/results.json`)
-    .then(r => r.json())
-    .then(data => {
-      return dispatch({type: "LOAD_RACE_DATA", payload: data.MRData.RaceTable.Races[0]})
+    dispatch({
+      type: "RACE_FETCH"
     })
+    return fetch(`http://ergast.com/api/f1/${season}/${round}/results.json`)
+      .then(r => r.json())
+      .then(data => {
+        return dispatch({
+          type: "LOAD_RACE_DATA",
+          payload: data.MRData.RaceTable.Races[0]
+        })
+      })
   }
 }
 
 export function fetchQualData(season, round) {
   return dispatch => {
     if (season === "current" || season > 2002) {
-      dispatch({type:"QUAL_FETCH"})
-      return fetch(`http://ergast.com/api/f1/${season}/${round}/qualifying.json`)
-      .then(r => r.json())
-      .then(data => {
-        return dispatch({type: "LOAD_QUAL_DATA", payload: data.MRData.RaceTable.Races[0].QualifyingResults})
+      dispatch({
+        type: "QUAL_FETCH"
       })
+      return fetch(`http://ergast.com/api/f1/${season}/${round}/qualifying.json`)
+        .then(r => r.json())
+        .then(data => {
+          return dispatch({
+            type: "LOAD_QUAL_DATA",
+            payload: data.MRData.RaceTable.Races[0].QualifyingResults
+          })
+        })
     } else {
-      return dispatch({type: "LOAD_QUAL_DATA", payload: ["Qualifying data not available prior to 2003 season."]})
+      return dispatch({
+        type: "LOAD_QUAL_DATA",
+        payload: ["Qualifying data not available prior to 2003 season."]
+      })
     }
 
   }
@@ -101,12 +134,18 @@ export function fetchPitData(season, round) {
   return dispatch => {
     if (season === "current" || season > 2011) {
       return fetch(`http://ergast.com/api/f1/${season}/${round}/pitstops.json`)
-      .then(r => r.json())
-      .then(data => {
-        return dispatch({type: "LOAD_PIT_DATA", payload: data.MRData.RaceTable.Races[0].PitStops})
-      })
+        .then(r => r.json())
+        .then(data => {
+          return dispatch({
+            type: "LOAD_PIT_DATA",
+            payload: data.MRData.RaceTable.Races[0].PitStops
+          })
+        })
     } else {
-      return dispatch({type: "LOAD_PIT_DATA", payload: ["Pit data not available prior to 2012 season."]})
+      return dispatch({
+        type: "LOAD_PIT_DATA",
+        payload: ["Pit data not available prior to 2012 season."]
+      })
     }
 
   }
@@ -114,13 +153,19 @@ export function fetchPitData(season, round) {
 
 export function setCircuitData(round) {
   return (dispatch, getState) => {
-    dispatch({type: "LOAD_CIRCUIT_DATA", payload: getState().seasonData.Races[round - 1].Circuit})
+    dispatch({
+      type: "LOAD_CIRCUIT_DATA",
+      payload: getState().seasonData.Races[round - 1].Circuit
+    })
   }
 }
 
 export function loadRacePreview(previewData) {
   return dispatch => {
-    dispatch({type: "LOAD_RACE_PREVIEW", payload: previewData})
+    dispatch({
+      type: "LOAD_RACE_PREVIEW",
+      payload: previewData
+    })
   }
 }
 
@@ -129,7 +174,9 @@ export function loadRacePreview(previewData) {
 export function createRacePreview(previewData) {
   //return dispatch => dispatch({type: "CREATE_RACE_PREVIEW", payload: previewData})
   return dispatch => {
-    dispatch({type: "CREATE_RACE_PREVIEW"})
+    dispatch({
+      type: "CREATE_RACE_PREVIEW"
+    })
     return Promise.all([
       dispatch(loadRacePreview(previewData)),
       dispatch(setCircuitData(previewData.round))
@@ -142,7 +189,10 @@ export function createRacePreview(previewData) {
 
 export function fetchGrandPrixData(data, type) {
   return (dispatch, getState) => {
-    dispatch({type: `LOAD_${type.toUpperCase()}_DATA`, payload: data})
+    dispatch({
+      type: `LOAD_${type.toUpperCase()}_DATA`,
+      payload: data
+    })
     let typeId
     type === "driver" ? typeId = data.driverId : typeId = data.constructorId
     return dispatch(fetchGrandPrixSeasonData(getState().season, typeId, type))
@@ -154,28 +204,28 @@ export function fetchGrandPrixSeasonData(season, id, type) {
     return Promise.all([
       loadRaceResults(season, id, type),
       loadQualResults(season, id, type)
-    ]).then(values => dispatch(combineRaceAndQualResults(values[0],values[1], type)))
+    ]).then(values => dispatch(combineRaceAndQualResults(values[0], values[1], type)))
 
   }
 }
 
 function loadRaceResults(season, id, type) {
   return fetch(`http://ergast.com/api/f1/${season}/${type}s/${id}/results.json`)
-  .then(r => r.json())
-  .then(data => data.MRData.RaceTable.Races)
+    .then(r => r.json())
+    .then(data => data.MRData.RaceTable.Races)
 }
 
 function loadQualResults(season, id, type) {
   return fetch(`http://ergast.com/api/f1/${season}/${type}s/${id}/qualifying.json`)
-  .then(r => r.json())
-  .then(data => data.MRData.RaceTable.Races)
+    .then(r => r.json())
+    .then(data => data.MRData.RaceTable.Races)
 }
 
 function combineRaceAndQualResults(raceResults, qualResults, type) {
   return dispatch => {
     let allRaceData = []
 
-    for (let i=0; i < raceResults.length; i++) {
+    for (let i = 0; i < raceResults.length; i++) {
       let raceObj = Object.assign(raceResults[i], qualResults[i])
       allRaceData.push(raceObj)
     }
@@ -184,6 +234,9 @@ function combineRaceAndQualResults(raceResults, qualResults, type) {
       allRaceData.push(qualResults.length - 1)
     }
 
-    return dispatch({type: `LOAD_${type.toUpperCase()}_SEASON_DATA`, payload: allRaceData})
+    return dispatch({
+      type: `LOAD_${type.toUpperCase()}_SEASON_DATA`,
+      payload: allRaceData
+    })
   }
 }
