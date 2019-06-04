2be2e2ca9799a09321691b520ff59d8db7e7a10d
Matt Migliore <mmigliore311@gmail.com>
2019-06-04 16:36:47 -0400

    add indent fix

--- src/components/DetailedResult/DetailedResult.js
+++ src/components/DetailedResult/DetailedResult.js
@@ -19,19 +19,19 @@ class DetailedResult extends Component {
     return (
       <div className="modal-container">
         <div className="modal-content container m-10">
-        <button onClick={this.props.closeDetailedResult}>Close</button>
-        <h2>{driver.givenName + " " + driver.familyName} <span>{driver.permanentNumber}</span></h2>
-        <h4>{constructor.name}</h4>
-        <h3>Results</h3>
-        <p><strong>Position: </strong> {result.position}</p>
-        <p><strong>Grid:</strong> {result.grid}</p>
-        <p><strong>Points:</strong> {result.points}</p>
-        <p><strong>Laps:</strong> {result.laps}</p>
-        <p><strong>Time:</strong> {result.status === "Finished" ? result.Time.time : "DNF"}</p>
-        <p><strong>Status:</strong> {result.status}</p>
-        {this.props.season === "current" || this.props.season > 2003 ? <FastestLap fastestLap={fastestLap}/> : null}
-        {this.props.season === "current" || this.props.season > 2011 ? pitstops.map(pit => <PitDetail key={pit.stop} pitData={pit} />) : null}
-      </div>
+          <button onClick={this.props.closeDetailedResult}>Close</button>
+          <h2>{driver.givenName + " " + driver.familyName} <span>{driver.permanentNumber}</span></h2>
+          <h4>{constructor.name}</h4>
+          <h3>Results</h3>
+          <p><strong>Position: </strong> {result.position}</p>
+          <p><strong>Grid:</strong> {result.grid}</p>
+          <p><strong>Points:</strong> {result.points}</p>
+          <p><strong>Laps:</strong> {result.laps}</p>
+          <p><strong>Time:</strong> {result.status === "Finished" ? result.Time.time : "DNF"}</p>
+          <p><strong>Status:</strong> {result.status}</p>
+          {this.props.season === "current" || this.props.season > 2003 ? <FastestLap fastestLap={fastestLap}/> : null}
+          {this.props.season === "current" || this.props.season > 2011 ? pitstops.map(pit => <PitDetail key={pit.stop} pitData={pit} />) : null}
+        </div>
       </div>
     );
   }
