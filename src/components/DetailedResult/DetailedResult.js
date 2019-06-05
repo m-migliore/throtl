ec139f41e9e22150c05b4164ef069e872044ca2f
Matt Migliore <mmigliore311@gmail.com>
2019-06-05 11:19:55 -0400

    add spacing in DetailedResult

--- src/components/DetailedResult/DetailedResult.js
+++ src/components/DetailedResult/DetailedResult.js
@@ -32,22 +32,24 @@ class DetailedResult extends Component {
 
     return (
       <div className="modal-container">
-        <div className="modal-content container my-10 mx-auto">
+        <div className="modal-content container my-10 mx-auto p-5">
           <button onClick={this.props.closeDetailedResult}>Close</button>
           <h2>{driver.givenName + " " + driver.familyName} <span>{driver.permanentNumber}</span></h2>
           <h4>{constructor.name}</h4>
-          <h3>Results</h3>
-          <p><strong>Position: </strong> {result.position}</p>
-          <p><strong>Grid:</strong> {result.grid}</p>
-          <p><strong>Points:</strong> {result.points}</p>
-          <p><strong>Laps:</strong> {result.laps}</p>
-          <p><strong>Time:</strong> {result.status === "Finished" ? result.Time.time : "DNF"}</p>
-          <p><strong>Status:</strong> {result.status}</p>
+          <div className="my-3">
+            <h3>Results</h3>
+            <p><strong>Position: </strong> {result.position}</p>
+            <p><strong>Grid:</strong> {result.grid}</p>
+            <p><strong>Points:</strong> {result.points}</p>
+            <p><strong>Laps:</strong> {result.laps}</p>
+            <p><strong>Time:</strong> {result.status === "Finished" ? result.Time.time : "DNF"}</p>
+            <p><strong>Status:</strong> {result.status}</p>
+          </div>
           {this.props.season === "current" || this.props.season > 2003 ? <FastestLap fastestLap={fastestLap}/> : null}
-          {/* {this.props.season === "current" || this.props.season > 2011 ? pitstops.map(pit => <PitDetail key={pit.stop} pitData={pit} />) : null} */}
-          <h3>Pit Stops</h3>
-          {this.pitLoad()}
-          {/* {this.props.season === "current" || this.props.season > 2011 ? <PitDetail /> : null} */}
+          <div className="my-3">
+            <h3>Pit Stops</h3>
+            {this.pitLoad()}
+          </div>
         </div>
       </div>
     );
--- src/components/DetailedResult/FastestLap.js
+++ src/components/DetailedResult/FastestLap.js
@@ -5,7 +5,7 @@ class FastestLap extends Component {
   render() {
     const fastestLap = this.props.fastestLap
     return (
-      <div>
+      <div className="my-3">
         <h3>Fastest Lap</h3>
         <p><strong>Time:</strong> {fastestLap.Time.time}</p>
         <p><strong>Average Speed:</strong> {fastestLap.AverageSpeed.speed + fastestLap.AverageSpeed.units}</p>
