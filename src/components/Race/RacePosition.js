9400255d9ab01bf7b12b8e27b32a3b4a5010cb23
Matt Migliore <mmigliore311@gmail.com>
2019-06-10 09:27:47 -0400

    add temp fix to driver with no lap postion

--- src/components/Race/RacePosition.js
+++ src/components/Race/RacePosition.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 class RacePosition extends Component {
   render() {
-    const posStyle = {
-      top: `${parseInt(this.props.lapInfo.lapInfo[this.props.lapNumber - 1].position) * 20}px`
+    let posStyle
+    if (this.props.lapInfo.lapInfo[this.props.lapNumber - 1]) {
+      posStyle = {
+        top: `${parseInt(this.props.lapInfo.lapInfo[this.props.lapNumber - 1].position) * 20}px`
+      }
+    } else {
+      posStyle = {
+        top: "800px"
+      }
     }
+    
 
     return (
       <div className="race-pos" style={posStyle}>
--- src/components/Race/WatchRace.js
+++ src/components/Race/WatchRace.js
@@ -11,18 +11,26 @@ class WatchRace extends Component {
 
   handleClick() {
     // while (this.state.lap < this.state.lapAmount) {
-    //   setTimeout(this.nextLap, 1500);
+    //   this.nextLap()
     // }
     this.setState((prevState, props) => ({
-      lap: prevState.lap += 1
+      lap: prevState.lap + 1
     }));
   }
 
-  nextLap(lapNumber) {
-    this.setState((prevState) => ({
-      lap: prevState.lap += 1
-    }));
-  }
+  // nextLap() {
+  //   this.setState((prevState) => ({
+  //     lap: prevState.lap += 1
+  //   }));
+  // }
+
+  // componentDidUpdate() {
+  //   if (this.state.lap > 1 && this.state.lap < this.state.lapAmount){
+  //     this.setState((prevState, props) => ({
+  //       lap: prevState.lap + 1
+  //     }));
+  //   }  
+  // }
 
 
   render() {
@@ -33,26 +41,10 @@ class WatchRace extends Component {
     const drivers = laps[0].Timings.map(lap => lap.driverId)
     const lapBreakdown = []
 
-    // drivers.forEach(driver => {
-    //   lapBreakdown[driver] = laps.map(lap => {
-    //     let lapNumber = lap.number
-    //     let info = lap.Timings.find(timing => timing.driverId === driver)
-    //     return {
-    //       lap: lapNumber,
-    //       info: info
-    //     }
-    //   })
-    // })
-
     drivers.forEach(driver => {
       let driverLapBreakdown = laps.map(lap => {
-        let lapNumber = lap.number
         let info = lap.Timings.find(timing => timing.driverId === driver)
         return info
-        // return {
-        //   lap: lapNumber,
-        //   info: info
-        // }
       })
       lapBreakdown.push({
         driverId: driver,
@@ -69,12 +61,7 @@ class WatchRace extends Component {
         <h2>Lap {this.state.lap}</h2>
         <button onClick={this.handleClick.bind(this)}>Watch Race</button>
         <div className="watch-race">
-          {/* {laps[this.state.lap - 1].Timings.map((lap,idx) => {
-            const posStyle = {
-              top: `${(idx + 1) * 20}px`
-            }
-            return <p className="race-pos" style={posStyle}>{lap.driverId}</p>
-          })} */}
+  
 
           {lapBreakdown.map(lap => <RacePosition lapInfo={lap} lapNumber={this.state.lap}/>)}
         </div>
