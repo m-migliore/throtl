b1c7e53cbdab62a295a6190a96dd43ac09ef5ec9
Matt Migliore <mmigliore311@gmail.com>
2019-06-13 15:56:35 -0400

    add lap interval in RacePosition

--- src/components/RaceReplay/RacePosition.js
+++ src/components/RaceReplay/RacePosition.js
@@ -4,6 +4,12 @@ import RacePositionIndicator from './RacePositionIndicator'
 
 class RacePosition extends Component {
 
+  convertTimeToSeconds(stringTime) {
+    const timeArray = stringTime.split(":")
+    const minuteValue = parseInt(timeArray[0]) * 60
+    return minuteValue + parseFloat(timeArray[1])
+  }
+
   render() {
     const lapData = this.props.lapData
     const driver = lapData.result.Driver
@@ -39,6 +45,18 @@ class RacePosition extends Component {
       }
     }
 
+    // Determine if lap leader, and calculate interval
+
+    const leader = this.props.lapData[this.props.replayLap].Timings[0]
+    const leaderTimeInSeconds = this.convertTimeToSeconds(leader.time)
+    const driverTimeInSeconds = this.convertTimeToSeconds(lapInfo[this.props.replayLap].time)
+    const isLeader = leader.driverId === driver.driverId
+    let lapInterval
+
+    if(!isLeader) {
+      lapInterval = leaderTimeInSeconds - driverTimeInSeconds
+    }
+  
     let fastestLapRank = parseInt(result.FastestLap.rank)
     let fastestLapNumber = parseInt(result.FastestLap.lap)    
 
@@ -46,6 +64,7 @@ class RacePosition extends Component {
       <div className="race-pos" style={posStyle}>
         <p>
           {driver.givenName + " " + driver.familyName}
+          <span className="lap-interval">{isLeader ? leader.time : "+" + lapInterval}</span>
           {fastestLapRank === 1 && this.props.replayLap >= fastestLapNumber ? <RacePositionIndicator iType={"fastest-lap"} message={`Fastest Lap - ${fastestLapNumber}`} /> : null}
           {pits.map(pit => parseInt(pit.lap)).includes(this.props.replayLap) && <RacePositionIndicator iType={"pit-stop"} message={"Pit Stop"} />}
           {dnfStatus.lap <= this.props.replayLap && <RacePositionIndicator iType={dnfStatus.status} message={dnfStatus.details} />}
