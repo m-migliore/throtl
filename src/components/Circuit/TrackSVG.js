b387688fedaedf338a2b15a29469424727484df2
Matt Migliore <mmigliore311@gmail.com>
2019-07-02 14:07:40 -0400

    add working track svg in circuit info

--- src/components/Circuit/TrackSVG.js
+++ src/components/Circuit/TrackSVG.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { trackpaths } from '../../helpers/trackpaths'
+import React, { Component } from 'react'
+import {trackpaths} from '../../helpers/trackpaths'
 
-const TrackSVG = (props) => {
-  const parser = new DOMParser()
-  const trackPath = parser.parseFromString(trackpaths[props.trackName], "text/html")
-  return (
-     <svg width="600" height="300" viewBox="0 0 500 350">
-       {trackPath}
-     </svg>
-  )
+class TrackSVG extends Component {
+  componentDidMount() {
+    const holder = document.getElementById("svg-holder")
+    holder.innerHTML = `
+    <svg width="600" height="300" viewBox="0 0 500 350">
+     ${trackpaths[this.props.trackName]}
+    </svg>`
+  }
+  render() {
+    return (
+      <div id="svg-holder"></div>
+    )
+  }
 }
 
 export default TrackSVG
