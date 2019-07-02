09c0f99fbaacf5942124a7dd9c8a1913e6c34541
Matt Migliore <mmigliore311@gmail.com>
2019-07-02 11:37:50 -0400

    cleanup RaceReplayContainer

--- src/components/RaceReplay/RaceReplayContainer.js
+++ src/components/RaceReplay/RaceReplayContainer.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchLapData } from "../../actions/actionCreators";
 import RaceReplayModal from './RaceReplayModal'
-import RaceReplayRace from "./RaceReplayRace";
-import ReplayStart from './ReplayStart'
 
 class RaceReplayContainer extends Component {
 	
@@ -11,44 +9,12 @@ class RaceReplayContainer extends Component {
 		this.props.fetchLapData(this.props.season, this.props.selectedRound);
 	}
 
-	// componentDidUpdate() {
-	// 	if (this.props.replayLap === this.props.lapData.length - 1) {
-	// 		clearInterval(this.lapInterval);
-	// 	}
-
-	// 	if (this.props.replayCountdown === 0 && this.props.replayLap === 0 && this.props.replayStart) {
-	// 		this.lapInterval = setInterval(() => this.props.nextLap(this.props.replayLap + 1), 1000);
-	// 	}
-	// }
-
-	// componentWillUnmount() {
-	// 	clearInterval(this.lapInterval)
-	// }
-
-
 	render() {
-		//const replayLap = this.props.replayLap
-
-		// if (this.props.lapData.length > 0) {
-		// 	return (
-		// 		<div className="my-5">
-		// 			<ReplayStart />
-		// 			{replayLap > 0 && replayLap !== this.props.lapData.length - 1 ? <h2>{`Lap ${replayLap}`}</h2> : null}
-		// 			{replayLap === this.props.lapData.length - 1 ? <h2 className="animate-pulse">Finished</h2> : null}
-		// 			<RaceReplayRace />
-		// 		</div>
-
-		// 	);
-		// } else if (this.props.lapDataLoading) {
-		// 	return <h2 className="my-5">Loading Lap Data</h2>;
-		// } else {
-		// 	return <h2 className="my-5">Lap Data Not Found</h2>;
-		// }
-	return (
-		<>
-		{this.props.replayView ? <RaceReplayModal /> : <button onClick={this.props.viewRaceReplay}>Watch Replay</button>  }
-		</>
-	)
+		return (
+			<>
+				{this.props.replayView ? <RaceReplayModal /> : <button onClick={this.props.viewRaceReplay}>Watch Replay</button>  }
+			</>
+		)
 	}
 }
 
@@ -56,11 +22,6 @@ const mapStateToProps = state => {
 	return {
 		season: state.season,
 		selectedRound: state.selectedRound,
-		// replayStart: state.replayStart,
-		// replayLap: state.replayLap,
-		// replayCountdown: state.replayCountdown,
-		// lapData: state.lapData,
-		// lapDataLoading: state.lapDataLoading,
 		replayView: state.replayView
 	};
 };
